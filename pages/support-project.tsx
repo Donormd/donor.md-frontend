0ad531a124d18513bceb94eb55003f7d02d80d6e
Maxim Lambov <maxilamb@yandex.com>
2021-01-06 15:25:58 +0200

    mend

--- pages/support-project.tsx
+++ pages/support-project.tsx
@@ -6,40 +6,40 @@ const SupportProject: React.FC = (): JSX.Element => (
   <HeaderContentFooter classNames={style.support}>
     <section className='container'>
       <h1 className={style.support__heading}>Поддержать проект donor.md</h1>
-      <p className={style.support__text}>
+      <p>
         Web-сервис donor.md создан как социальная инициатива по решению вопроса обеспечения
         Приднестровья донорской крови и развития донорского сообщества. Мы применяем самые последние
         технологические решения в сфере IT-разработок, но наш всё-таки зависит от тех, кто разделяет
         концепцию “Люди помогают людям”.
       </p>
-      <p className={style.support__text}>
+      <p>
         Мы всегда рады любой поддержке, которые объединит наши усилия, так как вместе мы сделаем
         больше.
       </p>
-      <p className={style.support__text}>
+      <p>
         Мы приглашаем Pro bono волонтеров и специалистов сделать вклад в развитие сервиса.
         Волонтерство ваших сотрудников или услуги вашей компании могут&nbsp;
         <Link href='/become-volunteer'>
           <a className={style.link__bold}>помочь усилить возможности</a>
         </Link>
       </p>
-      <p className={style.support__text}>
+      <p>
         Мы приглашаем коммерческих агентов, начинающих и крупных, стать участниками нашей&nbsp;
         <Link href='/bonus-program'>
           <a className={style.link__bold}>бонусной программы для доноров</a>
         </Link>
       </p>
-      <p className={style.support__text}>
+      <p>
         Мы приглашаем организации любых форм собственности стать участниками нашей программы&nbsp;
         <Link href='/corporate-donation'>
           <a className={style.link__bold}>“Корпоративной донорство”</a>
         </Link>
         .
       </p>
-      <p className={style.support__text}>
+      <p>
         Мы приглашаем заинтересованных людей и компаний помочь нам финансово из любого уголка мира.
       </p>
-      <p className={style.support__text}>
+      <p>
         Нам нужны ресурсы для запуска дополнительных функции донорского кабинета, создание промо
         материалов, технической поддержке проекта.
       </p>
