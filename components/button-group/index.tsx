6931a527a27e1868b3daf6357b37dad4daf61611
Maxim Lambov <maxilamb@yandex.com>
2021-01-07 02:38:01 +0200

    removed useCallback

--- components/button-group/index.tsx
+++ components/button-group/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Button } from 'antd';
 import cn from 'classnames';
 import style from './style.module.scss';
