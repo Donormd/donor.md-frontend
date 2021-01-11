36f7238ee984549be57bd5f950445656bfc68403
Maxim Lambov <maxilamb@yandex.com>
2021-01-11 20:07:29 +0200

    rewrote the styled component

--- components/logo/index.tsx
+++ components/logo/index.tsx
@@ -1,16 +1,39 @@
 import React from 'react';
-import style from './style.module.scss';
+import Image from 'next/image';
+import styled from 'styled-components';
+import { Paragraph } from '../UI/typography';
+
+const WrapperLogo = styled.div`
+  display: grid;
+  grid-template-columns: min-content 1fr;
+  column-gap: 10px;
+`;
+
+const WrapperImage = styled.div`
+  grid-row: span 2;
+  width: 45px;
+  height: 45px;
+`;
+
+const WrappedParagraph = styled(Paragraph)`
+  text-transform: uppercase;
+  margin: 0;
+  line-height: 1;
+`;
 
 const Logo: React.FC = (): JSX.Element => (
-  <div className={style.logo}>
-    <img
-      className={style.logo__image}
-      src='/images/logo/heart.svg'
-      alt='Web-платформа для тех, кто сдает и ищет донорскую кровь'
-    />
-    <p className={style.logo__brand}>donor.md</p>
-    <p className={style.logo__slogan}>Люди помогают людям</p>
-  </div>
+  <WrapperLogo>
+    <WrapperImage>
+      <Image
+        src='/images/logo/heart.svg'
+        width={45}
+        height={45}
+        alt='Web-платформа для тех, кто сдает и ищет донорскую кровь'
+      />
+    </WrapperImage>
+    <WrappedParagraph bold>donor.md</WrappedParagraph>
+    <WrappedParagraph>Люди помогают людям</WrappedParagraph>
+  </WrapperLogo>
 );
 
 export default Logo;
