a2a47502ca877fc2012fee45ec3ab1bd52d25dc0
Maxim Lambov <maxilamb@yandex.com>
2021-01-11 17:23:38 +0200

    added theme

---
+++ components/UI/theme.js
@@ -0,0 +1,14 @@
+const theme = {
+  red: '#ff434b',
+  redDark: '#CC353D',
+  redDiluted: '#f8bab4',
+  background: '#fffafa',
+  backgroundSm: '#fff7f4',
+  primary: '#ffecdd',
+  textDark: '#333333',
+  textGray: '#595757',
+  textMuted: '#808080',
+  radius: '25px',
+};
+
+export default theme;
