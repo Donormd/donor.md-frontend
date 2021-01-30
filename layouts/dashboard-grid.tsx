2a77572296faea73ef9b185d1cf0f446d00f06f9
Maxim Lambov <maxilamb@yandex.com>
2021-01-30 02:15:45 +0200

    added props for left aside
    added backgroung image for left aside
    added images for menu items

--- layouts/dashboard-grid.tsx
+++ layouts/dashboard-grid.tsx
@@ -5,25 +5,25 @@ import RightMenu from '../components/right-menu';
 
 type Props = {
   children: React.ReactNode;
-  background?: string;
+  leftImage?: string;
   className?: string;
 };
 
 const Main = styled.main`
   display: grid;
-  grid-template-columns: min-content 1fr min-content;
+  grid-template-columns: max-content 1fr max-content;
   height: 100%;
 `;
 
 const Container = styled.section`
-  padding: 35px;
+  padding: 25px;
   width: 100%;
   max-width: 1140px;
 `;
 
-const DashboardGrid: React.FC<Props> = ({ children, className }): JSX.Element => (
+const DashboardGrid: React.FC<Props> = ({ children, className, leftImage }): JSX.Element => (
   <Main>
-    <LeftMenu />
+    <LeftMenu image={leftImage} />
     <Container className={className}>{children}</Container>
     <RightMenu />
   </Main>
