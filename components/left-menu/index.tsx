bed1801cf85cba86db68f527029497c0acdf3986
Maxim Lambov <maxilamb@yandex.com>
2021-01-30 02:13:37 +0200

    added menu items

--- components/left-menu/index.tsx
+++ components/left-menu/index.tsx
@@ -1,12 +1,94 @@
 import React from 'react';
-import styled from 'styled-components';
+import Link from 'next/link';
+import { Tooltip } from 'antd';
+import { MenuItemProps, MenuItem, IconWrapper, Paragraph, Aside, Menu } from './styles';
+import { ResponsiveLogo } from '../logo';
+import Details from '../../public/images/pages/dashboard/left-aside/details.svg';
+import Donations from '../../public/images/pages/dashboard/left-aside/donations.svg';
+import Reviews from '../../public/images/pages/dashboard/left-aside/reviews.svg';
+import Settings from '../../public/images/pages/dashboard/left-aside/settings.svg';
+import Bonuses from '../../public/images/pages/dashboard/left-aside/bonuses.svg';
+import Recipients from '../../public/images/pages/dashboard/left-aside/recipients.svg';
+import Logout from '../../public/images/pages/dashboard/left-aside/logout.svg';
 
-const Aside = styled.aside`
-  display: none;
-`;
+export declare type MenuLinkProps = {
+  key: string | number;
+  Icon: any;
+  href: string;
+  text: string;
+};
 
-const LeftMenu: React.FC = (): JSX.Element => {
-  return <Aside>Left Menu</Aside>;
+export const mock: Array<MenuLinkProps> = [
+  {
+    key: 1,
+    Icon: Details,
+    href: '/dashboard/details',
+    text: 'Мои данные',
+  },
+  {
+    key: 2,
+    Icon: Donations,
+    href: '/dashboard/donations',
+    text: 'Мои донации',
+  },
+  {
+    key: 3,
+    Icon: Reviews,
+    href: '/dashboard/reviews',
+    text: 'Мои отзывы',
+  },
+  {
+    key: 4,
+    Icon: Settings,
+    href: '/dashboard/settings',
+    text: 'Настройки',
+  },
+  {
+    key: 5,
+    Icon: Bonuses,
+    href: '/dashboard/bonuses',
+    text: 'Бонусы',
+  },
+  {
+    key: 6,
+    Icon: Recipients,
+    href: '/dashboard/recipients',
+    text: 'Реципиенты',
+  },
+  {
+    key: 7,
+    Icon: Logout,
+    href: '/dashboard/logout',
+    text: 'Выход',
+  },
+];
+
+const MenuLink: React.FC<MenuLinkProps & MenuItemProps> = ({ active, Icon, href, text }) => (
+  <Tooltip title={text} placement='left'>
+    <MenuItem active={active}>
+      <Link href={href}>
+        <>
+          <IconWrapper>
+            <Icon />
+          </IconWrapper>
+          <Paragraph>{text}</Paragraph>
+        </>
+      </Link>
+    </MenuItem>
+  </Tooltip>
+);
+
+const LeftMenu: React.FC<{ image?: string }> = ({ image }): JSX.Element => {
+  return (
+    <Aside image={image}>
+      <ResponsiveLogo />
+      <Menu>
+        {mock.map((item, i) => (
+          <MenuLink active={i === 0} {...item} />
+        ))}
+      </Menu>
+    </Aside>
+  );
 };
 
 export default LeftMenu;
---
+++ components/left-menu/styles.ts
@@ -0,0 +1,64 @@
+import styled from 'styled-components';
+
+export declare type MenuItemProps = { active: boolean };
+
+export const Aside = styled.aside<{ image?: string }>`
+  padding: 25px;
+  background: white;
+  border-right: 1px solid ${({ theme }) => theme.redDiluted};
+
+  @media (min-width: 992px) {
+    background-image: ${({ image }) => `url(${image})`};
+    background-size: 100% 24vw;
+    background-repeat: no-repeat;
+    background-position: left bottom;
+  }
+`;
+
+export const Menu = styled.ul`
+  margin-top: 30px;
+  list-style-type: none;
+  padding: 0;
+`;
+export const MenuItem = styled.li<MenuItemProps>`
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  padding: 10px 20px;
+  margin-bottom: 5px;
+  color: ${({ active }) => (active ? 'white' : 'black')};
+  & svg path {
+    fill: ${({ active }) => (active ? 'white' : 'black')};
+  }
+  border-radius: ${({ theme }) => theme.radius};
+  background: ${({ theme, active }) => (active ? theme.red : 'white')};
+
+  &:hover {
+    color: white;
+
+    background: ${({ theme, active }) => (active ? theme.redDark : theme.redDiluted)};
+  }
+
+  &:hover svg path {
+    fill: white;
+  }
+`;
+
+export const Paragraph = styled.p`
+  display: none;
+  @media (min-width: 992px) {
+    display: inline;
+    margin: 0;
+  }
+`;
+
+export const IconWrapper = styled.div`
+  &,
+  & svg {
+    width: 30px;
+    height: 30px;
+  }
+  @media (min-width: 992px) {
+    margin-right: 10px;
+  }
+`;
