91646c3c344e3d857a2820aeed50b5d3381b28b5
Maxim Lambov <maxilamb@yandex.com>
2021-02-01 13:58:54 +0200

    added links

--- components/right-menu/index.tsx
+++ components/right-menu/index.tsx
@@ -1,12 +1,74 @@
 import React from 'react';
-import styled from 'styled-components';
+import Link from 'next/link';
+import { Aside, Menu, MenuItem, LinkGrid, IconWrapper, ItemParagraph } from './styles';
+import Schedule from '../../public/images/pages/dashboard/right-aside/schedule-donation.svg';
+import Add from '../../public/images/pages/dashboard/right-aside/add-donation.svg';
+import Question from '../../public/images/pages/dashboard/right-aside/question.svg';
+import Support from '../../public/images/pages/dashboard/right-aside/support-project.svg';
+import DonorInfo from '../donor-info';
+import DonorProgress from '../donor-progress';
+import DonorCard from '../donor-card';
 
-const Aside = styled.aside`
-  display: none;
-`;
+export declare type MenuLinkProps = {
+  key: string | number;
+  Icon: any;
+  href: string;
+  text: string;
+};
+
+const data: Array<MenuLinkProps> = [
+  {
+    key: 1,
+    Icon: Schedule,
+    href: '/dashboard/details',
+    text: 'Запланировать донацию',
+  },
+  {
+    key: 2,
+    Icon: Add,
+    href: '/dashboard/donations',
+    text: 'Добавить донацию',
+  },
+  {
+    key: 3,
+    Icon: Question,
+    href: '/dashboard/reviews',
+    text: 'Ваш отзыв / вопрос',
+  },
+  {
+    key: 4,
+    Icon: Support,
+    href: '/support-project',
+    text: 'Поддержать проект',
+  },
+];
+
+const MenuLink: React.FC<MenuLinkProps> = ({ Icon, href, text }) => (
+  <MenuItem>
+    <Link href={href}>
+      <LinkGrid>
+        <ItemParagraph>{text}</ItemParagraph>
+        <IconWrapper>
+          <Icon />
+        </IconWrapper>
+      </LinkGrid>
+    </Link>
+  </MenuItem>
+);
 
 const RightMenu: React.FC = (): JSX.Element => {
-  return <Aside>Right Menu</Aside>;
+  return (
+    <Aside>
+      <DonorInfo />
+      <DonorCard />
+      <DonorProgress />
+      <Menu>
+        {data.map((link) => (
+          <MenuLink {...link} />
+        ))}
+      </Menu>
+    </Aside>
+  );
 };
 
 export default RightMenu;
---
+++ components/right-menu/styles.ts
@@ -0,0 +1,47 @@
+import styled from 'styled-components';
+import { Paragraph } from '../UI';
+export const Aside = styled.aside`
+  padding: 25px;
+  background: ${({ theme }) => theme.primary};
+`;
+
+export const Menu = styled.ul`
+  margin-top: 30px;
+  list-style-type: none;
+  padding: 0;
+`;
+export const MenuItem = styled.li`
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  margin-bottom: 15px;
+  padding: 0 5px 0 15px;
+  color: black;
+  overflow: hidden;
+  border-radius: ${({ theme }) => theme.radius};
+  border: ${({ theme }) => `1px solid ${theme.red}`};
+
+  &:hover {
+    background: ${({ theme }) => theme.redDiluted};
+  }
+
+  &:hover path {
+    stroke: white;
+  }
+`;
+
+export const LinkGrid = styled.div`
+  display: grid;
+  grid-template-columns: 120px min-content;
+`;
+
+export const IconWrapper = styled.div`
+  width: 90px;
+  height: 90px;
+`;
+
+export const ItemParagraph = styled(Paragraph)`
+  display: flex;
+  align-items: center;
+  margin: 0;
+`;
