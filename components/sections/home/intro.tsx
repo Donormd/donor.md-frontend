04c219865552c0d66b415f3fa10a3e5669bde7fd
Maxim Lambov <maxilamb@yandex.com>
2021-02-04 17:58:41 +0200

    refactored
    added css gateway for image width

--- components/sections/home/intro.tsx
+++ components/sections/home/intro.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import NextImage from 'next/image';
+import Image from 'next/image';
 import Link from 'next/link';
 import styled from 'styled-components';
 import { Title, Paragraph, Button } from '../../UI';
@@ -17,46 +17,68 @@ const Section = styled.section`
 const ImageWrapper = styled.div`
   width: 100%;
   margin: -1.5rem 0 -4rem 0;
+  @media (min-width: 992px) {
+    width: calc(0.98039 * 100vw + 627px);
+  }
+  @media (min-width: 1400px) {
+    width: 1950px;
+  }
 `;
 
-const BoldText = styled.b`
+const StyledTitle = styled(Title)`
+  line-height: 1;
+`;
+
+const RedText = styled.b`
   color: ${({ theme }) => theme.red};
+`;
+
+const Column = styled.div`
+  margin-top: 30px;
+`;
+
+const ColumnParagraph = styled(Paragraph)`
+  padding: 15px 0;
 `;
 
 const ButtonGroup = styled.div`
   display: grid;
-  grid-template-columns: repeat(2, 45%);
-  justify-content: space-between;
+  grid-template-columns: 1fr;
+  gap: 15px;
+  @media (min-width: 360px) {
+    grid-template-columns: 1fr 1fr;
+  }
 
   @media (min-width: 992px) {
     grid-template-columns: min-content;
-    row-gap: 20px;
   }
 `;
 
 const Intro: React.FC = (): JSX.Element => (
   <Section id='intro'>
     <ImageWrapper>
-      <NextImage src='/images/pages/home/love.svg' width={300} height={300} layout='responsive' />
+      <Image src='/images/pages/home/love.svg' width={300} height={300} layout='responsive' />
     </ImageWrapper>
-    <div>
-      <Title bold>
-        <BoldText>DONOR.MD</BoldText> - место, где люди помогают людям
-      </Title>
-      <Paragraph>Web-сервис для тех, кто сдает и ищет донорскую кровь в Приднестровье</Paragraph>
+    <Column>
+      <StyledTitle bold>
+        <RedText>DONOR.MD</RedText> - место, где люди помогают людям
+      </StyledTitle>
+      <ColumnParagraph>
+        Web-сервис для тех, кто сдает и ищет донорскую кровь в Приднестровье
+      </ColumnParagraph>
       <ButtonGroup>
-        <Link href='/need-blood'>
-          <Button size='large' shape='round' color='primary' outlined>
-            Нужна кровь
-          </Button>
-        </Link>
         <Link href='/become-donor'>
           <Button size='large' shape='round' color='red' outlined>
             Стать донором
           </Button>
         </Link>
+        <Link href='/need-blood'>
+          <Button size='large' shape='round' color='primary' outlined>
+            Нужна кровь
+          </Button>
+        </Link>
       </ButtonGroup>
-    </div>
+    </Column>
   </Section>
 );
 
