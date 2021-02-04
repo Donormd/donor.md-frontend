08c1d8f71dc0aa00f92bd379ffd7e1605cac5bc8
Maxim Lambov <maxilamb@yandex.com>
2021-02-04 18:03:17 +0200

    changed path on AboutDonation

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import { Container } from '../layouts/container';
 import Intro from '../components/sections/home/intro';
 import Monitoring from '../components/sections/home/monitoring';
 import Places from '../components/sections/home/places';
-import AboutDonation from '../components/sections/home/about-donation';
+import AboutDonation from '../components/sections/home/about-donations';
 import CallToAction from '../components/sections/home/call-to-action';
 import Stories from '../components/sections/home/stories';
 import Feedback from '../components/sections/home/feedback';
