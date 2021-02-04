215ec85358eeb771688a1a22ad74c1c77b407d56
Maxim Lambov <maxilamb@yandex.com>
2021-02-04 17:42:19 +0200

    refactored
    changed the breakpoints
    moved the styles to a separate file

--- components/sections/home/call-to-action/index.tsx
+++ components/sections/home/call-to-action/index.tsx
@@ -1,52 +1,13 @@
 import React from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
-import styled from 'styled-components';
-import { Title, Button, StyledLink } from '../../UI';
-import { Section } from './utils';
-
-const StyledTitle = styled(Title)`
-  display: flex;
-`;
-
-const ImageWrapper = styled.div`
-  @media (max-width: 768px) {
-    display: none;
-  }
-`;
-
-const Underline = styled(StyledLink)`
-  margin-right: 20px;
-  color: ${({ theme }) => theme.red};
-  text-decoration: underline;
-`;
-
-const ArticleGrid = styled.div`
-  @media (min-width: 992px) {
-    display: grid;
-    grid-template-columns: repeat(2, 1fr);
-    column-gap: 30px;
-  }
-`;
-
-const Article = styled.article`
-  display: grid;
-  grid-template-columns: 1fr;
-  gap: 20px;
-  padding: 25px;
-  margin-bottom: 30px;
-  border: ${({ theme }) => `1px solid ${theme.redDiluted}`};
-  border-radius: ${({ theme }) => theme.radius};
-  background: white;
-
-  @media (min-width: 576px) {
-    grid-template-columns: min-content 1fr;
-  }
-`;
+import { Title, Button } from '../../../UI';
+import { Section } from '../utils';
+import { SectionTitle, Underline, ImageWrapper, ArticleGrid, Article } from './styles';
 
 const CallToAction: React.FC = (): JSX.Element => (
   <Section id='call-to-action'>
-    <StyledTitle as='h2' className='h1' bold>
+    <SectionTitle as='h2' className='h1' bold>
       С нами уже{' '}
       <Link href='/top-donors'>
         <Underline>105 доноров</Underline>
@@ -54,7 +15,7 @@ const CallToAction: React.FC = (): JSX.Element => (
       <ImageWrapper>
         <Image src='/images/pages/home/top-donors.png' width={170} height={30} />
       </ImageWrapper>
-    </StyledTitle>
+    </SectionTitle>
     <ArticleGrid>
       <Article>
         <Link href='/dashboard'>
