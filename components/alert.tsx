251d122d998877ee29524725fb7ac1a4f415c8fa
Maxim Lambov <maxilamb@yandex.com>
2021-02-22 22:20:23 +0200

    added dismissible flag

--- components/alert.tsx
+++ components/alert.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
+import { Paragraph } from './UI';
 
 export declare type Props = {
   dismissible?: boolean;
@@ -8,18 +9,59 @@ export declare type Props = {
   children?: React.ReactNode;
 };
 
-const StyledMessage = styled.div`
+const ButtonWrapper = styled.div`
+  align-self: center;
+`;
+
+const Line = styled.div`
+  width: 100%;
+  height: 2px;
+  background: ${({ theme }) => theme.textDark};
+`;
+
+const CloseIcon = styled.div`
+  cursor: pointer;
+  min-width: 30px;
+  height: 30px;
+
+  ${Line}:nth-of-type(1) {
+    transform: rotate(45deg) translate(8px, 9px);
+  }
+  ${Line}:nth-of-type(2) {
+    transform: rotate(-45deg) translate(-7px, 7px);
+  }
+`;
+
+const Message = styled.div<{ hide: boolean }>`
+  display: ${({ hide }) => (hide ? 'none' : 'flex')};
   margin: 20px 0;
   padding: 15px 25px;
   border: 1px solid black;
   border-radius: ${({ theme }) => `${theme.radius} ${theme.radius} ${theme.radius} 0`};
   box-shadow: 0 25px 25px -25px rgba(0, 0, 0, 0.25);
   background: white;
+
+  ${CloseIcon} {
+    margin-left: 15px;
+  }
 `;
 
-const Alert: React.FC<Props> = ({ message = '', children, className }): JSX.Element => {
-  if (children) return <StyledMessage className={className}>{children}</StyledMessage>;
-  return <StyledMessage className={className}>{message}</StyledMessage>;
+const Alert: React.FC<Props> = ({ message, children, className, dismissible }): JSX.Element => {
+  const [hide, setHide] = useState<boolean>(!dismissible || false);
+
+  return (
+    <Message hide={hide}>
+      <Paragraph className={className}>{message || children}</Paragraph>
+      {dismissible && (
+        <ButtonWrapper>
+          <CloseIcon arial-role='button' onClick={() => setHide(true)}>
+            <Line />
+            <Line />
+          </CloseIcon>
+        </ButtonWrapper>
+      )}
+    </Message>
+  );
 };
 
 export default React.memo(Alert);
