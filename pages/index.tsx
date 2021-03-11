28f9c26fddc2eba3cd2dd5922e46845b54c89225
Maxim Lambov <maxilamb@yandex.com>
2021-03-12 01:01:48 +0200

    refactored

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import Header from '../components/common/header';
 import Footer from '../components/common/footer';
 import { Container } from '../layouts/container';
@@ -8,21 +9,25 @@ import AboutDonation from '../components/sections/home/about-donations';
 import CallToAction from '../components/sections/home/call-to-action';
 import Stories from '../components/sections/home/stories';
 import Feedback from '../components/sections/home/feedback';
+import { useAppSelector } from '../redux/store';
 
-const HomePage: React.FC = (): JSX.Element => (
-  <>
-    <Header />
-    <Container as='main'>
-      <Intro />
-      <Monitoring />
-      <Places />
-      <AboutDonation />
-      <CallToAction />
-      <Stories />
-      <Feedback />
-    </Container>
-    <Footer />
-  </>
-);
+const HomePage: React.FC = (): JSX.Element => {
+  const { home: data } = useAppSelector((state) => state.aboutDonations);
+  return (
+    <>
+      <Header />
+      <Container as='main'>
+        <Intro />
+        <Monitoring />
+        <Places />
+        <AboutDonation title='Подготовка к первой сдачи крови' data={data} />
+        <CallToAction />
+        <Stories />
+        <Feedback />
+      </Container>
+      <Footer />
+    </>
+  );
+};
 
 export default HomePage;
