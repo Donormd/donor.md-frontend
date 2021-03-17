1fa05a3e027cf07de560695da92b545daba385f1
Maxim Lambov <maxilamb@yandex.com>
2021-03-17 23:17:37 +0200

    added fetching data

--- components/sections/home/monitoring/index.tsx
+++ components/sections/home/monitoring/index.tsx
@@ -1,23 +1,41 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import { Paragraph, Title } from '../../../UI';
 import { Section } from '../utils';
 import { BloodList } from './styles';
 import BloodItem from './blood-item';
 import { useAppSelector } from '../../../../redux/store';
+import { IBlood, getData } from '../../../../redux/redusers/monitoring';
 
 const Monitoring: React.FC = (): JSX.Element => {
-  const { data } = useAppSelector((state) => state.monitoring);
+  const { data, status } = useAppSelector((state) => state.monitoring);
+
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getData());
+  }, []);
+
+  const options = {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  };
+
+  if (status === 'loading') return <h1>loading</h1>;
+
+  const dateUpdate = new Date(data[0].dateUpdate).toLocaleString('ru', options);
+
   return (
     <Section id='monitoring'>
       <Title as='h2' className='h1' bold>
         Мониторинг запасов
       </Title>
       <BloodList>
-        {data.map((item) => (
-          <BloodItem {...item} />
+        {data.map((item: IBlood) => (
+          <BloodItem key={item._id} {...item} />
         ))}
       </BloodList>
-      <Paragraph align='right'>По состоянию на: 24.07.2020</Paragraph>
+      <Paragraph align='right'>По состоянию на: {dateUpdate}</Paragraph>
     </Section>
   );
 };
