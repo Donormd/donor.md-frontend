883ca290f7b674b25cf17aac8ca6b43424df1895
Maxim Lambov <maxilamb@yandex.com>
2021-03-21 15:33:15 +0200

    added a hook for routing in the donor's office

---
+++ hooks/useRequiredAuth.ts
@@ -0,0 +1,15 @@
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import { useAuth } from './useAuth';
+
+export const useRequiredAuth = (resolve: string | null, reject: string) => {
+  const auth = useAuth();
+  const router = useRouter();
+  useEffect(() => {
+    if (!auth.user) {
+      router && router.push(reject);
+    } else {
+      router && router.push(resolve || router.pathname);
+    }
+  }, []);
+};
