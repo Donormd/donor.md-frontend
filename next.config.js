eeda15e0a2fda13eda060583ed1a062d231659ce
Maxim Lambov <maxilamb@yandex.com>
2021-03-21 22:31:45 +0200

    update and added pwa

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
+const withPWA = require('next-pwa');
+const runtimeCaching = require('next-pwa/cache');
+
 module.exports = {
   poweredByHeader: false,
   reactStrictMode: true,
   images: {
     deviceSizes: [576, 768, 992, 1140],
+  },
+  pwa: {
+    dest: 'public',
+    runtimeCaching,
   },
 };
