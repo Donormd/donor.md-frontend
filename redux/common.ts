cbce7c9170c4eb379ca5bcd20c1f9339c63aade4
Maxim Lambov <maxilamb@yandex.com>
2021-03-29 23:30:48 +0300

    added loading options

--- redux/common.ts
+++ redux/common.ts
@@ -1,61 +1,81 @@
-/* eslint no-console:0 */
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import axios from 'axios';
 import { IState } from '../interfaces/initial-state';
+import { apiV1 } from './constants/url';
 
 export interface IOptions {
   value: string | number;
   text: string | number;
 }
 
-interface IData {
-  cities: IOptions[] | null;
-  bloodGroups: IOptions[] | null;
-  bloodCenter: IOptions[] | null;
-  organizations: IOptions[] | null;
-  transfusionCenter: IOptions[] | null;
-  typesAssistance: IOptions[] | null;
-}
-
-const initialState: IState<IData> = {
+const insideState: IState<IOptions[] | null> = {
   status: 'init',
-  data: {
-    cities: null,
-    bloodGroups: null,
-    bloodCenter: null,
-    organizations: null,
-    transfusionCenter: null,
-    typesAssistance: null,
-  },
+  data: null,
   error: null,
 };
 
-const commonMap = {
-  cities: '',
-  bloodGroups: '',
-  bloodCenter: '',
-  organizations: '',
-  transfusionCenter: '',
-  typesAssistance: '',
+const initialState: Record<string, IState<IOptions[] | null>> = {
+  cities: insideState,
+  bloodGroups: insideState,
+  bloodCenter: insideState,
+  organizations: insideState,
+  transfusionCenter: insideState,
+  typesAssistance: insideState,
+  userGroup: insideState,
+  userStatus: insideState,
+  sex: insideState,
 };
 
-const getData = createAsyncThunk<IOptions[], keyof IData>('common/get', async (dataType) => {
-  const response = await axios.get(commonMap[dataType]);
-  return response.data;
-});
+type Options =
+  | 'cities'
+  | 'bloodGroups'
+  | 'bloodCenter'
+  | 'organizations'
+  | 'transfusionCenter'
+  | 'typesAssistance'
+  | 'userGroup'
+  | 'userStatus'
+  | 'sex';
+
+const commonMap: Record<Options, string> = {
+  cities: `${apiV1}/city`,
+  bloodGroups: `${apiV1}/blood-group`,
+  bloodCenter: `${apiV1}/blood-center`,
+  organizations: `${apiV1}/organization`,
+  transfusionCenter: `${apiV1}/transfusion-сenter`,
+  typesAssistance: `${apiV1}/types-assistance`,
+  userGroup: `${apiV1}/user-group`,
+  userStatus: `${apiV1}/user-status`,
+  sex: `${apiV1}/sex`,
+};
+
+export const getOptions = createAsyncThunk<IOptions[], Options>(
+  'common/options/get',
+  async (dataType) => {
+    const response = await axios.get(commonMap[dataType]);
+    return response.data;
+  },
+);
 
 const common = createSlice({
   name: 'common',
   initialState,
   reducers: {},
   extraReducers: (builder) => {
-    builder.addCase(getData.fulfilled, (state, action) => {
-      console.log({ action });
-      state.status = 'success';
-      state.data = {
-        ...state.data,
-        cities: action.payload,
-      };
+    builder.addCase(getOptions.fulfilled, (state, action) => {
+      const { arg } = action.meta;
+      state[arg].status = 'success';
+      state[arg].data = action.payload;
+      state[arg].error = null;
+    });
+    builder.addCase(getOptions.pending, (state, action) => {
+      const { arg } = action.meta;
+      state[arg].status = 'loading';
+    });
+    builder.addCase(getOptions.rejected, (state, action) => {
+      const { arg } = action.meta;
+      state[arg].status = 'error';
+      state[arg].error = action.error.message || 'Ops something went wrong';
     });
   },
 });
