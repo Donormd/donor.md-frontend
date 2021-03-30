7bca45b3a84901e9b17e4dc066e20f17b860676b
Maxim Lambov <maxilamb@yandex.com>
2021-03-31 02:28:53 +0300

    updated url

--- redux/constants/url.ts
+++ redux/constants/url.ts
@@ -1,4 +1,5 @@
 const { NODE_ENV } = process.env;
 
-export const backend = NODE_ENV === 'development' ? 'http://localhost:5000' : '';
+export const backend =
+  NODE_ENV === 'development' ? 'http://localhost:5000' : 'https://donor-md-backend.herokuapp.com';
 export const apiV1 = `${backend}/api/v1`;
