6ef7215d55a3c9ffa3cd6ee343e5844268b44006
Maxim Lambov <maxilamb@yandex.com>
2021-04-05 01:36:10 +0300

    added react hook form

--- components/forms/auth/recovery.tsx
+++ components/forms/auth/recovery.tsx
@@ -1,24 +1,36 @@
-import React from 'react';
-import { Form, FormItem, Input, Title } from '../../UI';
+import React, { FC } from 'react';
+import { useForm } from 'react-hook-form';
+import { FormItem, Input, Title } from '../../UI';
 import { onChangeState } from './types';
 import { ActionLayout } from './utils';
+// import Alert from '../../alert';
 
 declare type Props = { onChangeState: onChangeState };
 
-export const RecoveryForm: React.FC<Props> = ({ onChangeState }): JSX.Element => (
-  <Form>
-    <Title as='h2' margin='15px'>
-      Восстановление доступа
-    </Title>
-    <FormItem columns={1}>
-      <Input placeholder='Введите email' />
-    </FormItem>
-    <div>
-      <ActionLayout
-        btnText='Восстановить'
-        linkText='Войти'
-        linkOnClick={() => onChangeState('signIn')}
-      />
-    </div>
-  </Form>
-);
+export const RecoveryForm: FC<Props> = ({ onChangeState }) => {
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = (data: { email: string }) => {
+    // eslint-disable-next-line no-console
+    console.log(data);
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <Title as='h2' margin='15px'>
+        Восстановление доступа
+      </Title>
+      <FormItem columns={1}>
+        <Input placeholder='Введите email' name='email' innerRef={register} />
+      </FormItem>
+      <div>
+        <ActionLayout
+          btnText='Восстановить'
+          linkText='Войти'
+          linkOnClick={() => onChangeState('signIn')}
+        />
+      </div>
+      {/* {status === 'success' && <Alert dismissible>{status}</Alert>} */}
+    </form>
+  );
+};
