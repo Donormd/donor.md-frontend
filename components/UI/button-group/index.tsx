374b28d77fe1a1a488bc40d495e2e0123953ba9e
Maxim Lambov <maxilamb@yandex.com>
2021-04-06 23:01:18 +0300

    added max-height

--- components/UI/button-group/index.tsx
+++ components/UI/button-group/index.tsx
@@ -3,44 +3,6 @@ import styled from 'styled-components';
 import { Button } from '../button';
 import { KeyType, ButtonsProps, Props } from './types';
 
-const ButtonsWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-
-  @media (min-width: 576px) {
-    flex-direction: row;
-  }
-
-  & button {
-    margin-bottom: 15px;
-  }
-`;
-
-const StyledButton = styled(Button)<{ active: boolean }>`
-  width: 100%;
-  color: ${({ active }) => active && 'white'};
-  background: ${({ active, theme }) => active && theme.colors.danger};
-
-  @media (min-width: ${({ theme }) => theme.media.sm}) {
-    &:first-child {
-      border-top-right-radius: 0;
-      border-bottom-right-radius: 0;
-      border-right: 0;
-    }
-
-    &:nth-of-type(2):not(:last-child) {
-      border-radius: 0;
-      border-right-width: 0;
-    }
-
-    &:last-child {
-      border-top-left-radius: 0;
-      border-bottom-left-radius: 0;
-    }
-  }
-`;
-
 const ButtonGroup: React.FC<Props> = ({ buttons, handleClick }): JSX.Element => {
   const [buttonId, setButtonId] = useState<KeyType>(0);
 
@@ -72,3 +34,42 @@ const ButtonGroup: React.FC<Props> = ({ buttons, handleClick }): JSX.Element => 
 
 export * from './types';
 export default React.memo(ButtonGroup);
+
+const ButtonsWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+
+  @media (min-width: 576px) {
+    flex-direction: row;
+    max-height: 62px;
+  }
+
+  & button {
+    margin-bottom: 15px;
+  }
+`;
+
+const StyledButton = styled(Button)<{ active: boolean }>`
+  width: 100%;
+  color: ${({ active }) => active && 'white'};
+  background: ${({ active, theme }) => active && theme.colors.danger};
+
+  @media (min-width: ${({ theme }) => theme.media.sm}) {
+    &:first-child {
+      border-top-right-radius: 0;
+      border-bottom-right-radius: 0;
+      border-right: 0;
+    }
+
+    &:nth-of-type(2):not(:last-child) {
+      border-radius: 0;
+      border-right-width: 0;
+    }
+
+    &:last-child {
+      border-top-left-radius: 0;
+      border-bottom-left-radius: 0;
+    }
+  }
+`;
