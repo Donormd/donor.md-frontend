cd3fb7cf217c3a998b7082618dd730b506cf80a4
maxilamb <maxilamb@yandex.com>
2021-05-12 22:53:47 +0300

    added sticky for aside menus

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,17 @@
-import React from 'react';
+import { FC } from 'react';
 import { AppProps } from 'next/app';
 import { ThemeProvider } from 'styled-components';
-import 'normalize.css';
 import 'antd/dist/antd.css';
+import 'normalize.css';
 import { Provider } from 'react-redux';
 import { theme } from '../components/UI/theme';
 import TypographyStyles from '../styles/typography';
 import GlobalStyles from '../styles/globals';
 import { store } from '../redux/store';
 import { AuthProvider } from '../hooks/useAuth';
+import { PlanningButton } from '../components/planning-button';
 
-const App: React.FC<AppProps> = ({ Component, pageProps }): JSX.Element => {
+const App: FC<AppProps> = ({ Component, pageProps }): JSX.Element => {
   return (
     <Provider store={store}>
       <ThemeProvider theme={theme}>
@@ -18,6 +19,7 @@ const App: React.FC<AppProps> = ({ Component, pageProps }): JSX.Element => {
           <GlobalStyles />
           <TypographyStyles />
           <Component {...pageProps} />
+          <PlanningButton />
         </AuthProvider>
       </ThemeProvider>
     </Provider>
