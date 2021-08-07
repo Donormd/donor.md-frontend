32b5c291d1a9a6db1b593a86f5233ac02f24d9b5
maxilamb <maxim.l@darqube.io>
2021-08-07 15:51:34 +0300

    updated logic

--- src/core/services/storage.ts
+++ src/core/services/storage.ts
@@ -1,46 +1,15 @@
-const DAY_MS: number = 1000 * 60 * 60 * 24;
-
-export interface IStorageItem<T> {
-  dateCreation: number;
-  data: T;
-}
-
-const get = <T>(key: string): IStorageItem<T> | null => {
+const get = <T>(key: string): T | null => {
   const stringValue = localStorage.getItem(key);
   return stringValue ? JSON.parse(stringValue) : null;
 };
 
-const setMemo = (key: string, data: unknown, expire = DAY_MS): void => {
-  const currentValue = get(key);
-
-  if (!currentValue) {
-    const objValue = JSON.stringify({ dateCreation: Date.now(), data });
-    localStorage.setItem(key, objValue);
-  } else {
-    const dateExpire = +new Date(currentValue.dateCreation + expire);
-    if (currentValue.dateCreation > dateExpire) {
-      const objValue = JSON.stringify({ dateCreation: Date.now(), data });
-      localStorage.setItem(key, objValue);
-    }
-  }
-};
-
-const set = (key: string, data: unknown): void => {
-  const objValue = JSON.stringify({ dateCreation: Date.now(), data });
+const set = <T>(key: string, data: T): void => {
+  const objValue = JSON.stringify(data);
   localStorage.setItem(key, objValue);
-};
-
-const isActualData = (key: string, expire = DAY_MS) => {
-  const currentValue = get(key);
-
-  if (!currentValue) return false;
-
-  const dateExpire = +new Date(currentValue.dateCreation + expire);
-  return currentValue.dateCreation <= dateExpire;
 };
 
 const remove = (key: string): void => {
   localStorage.removeItem(key);
 };
 
-export const storage = { get, set, setMemo, remove, isActualData };
+export const storage = { get, set, remove };
