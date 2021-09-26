84380705925f2b9902dffe6cecd66b6f6d682283
Maxim Lambov <maxilamb@yandex.com>
2021-09-26 23:33:40 +0300

    fix footer

--- src/components/common/footer/index.tsx
+++ src/components/common/footer/index.tsx
@@ -8,29 +8,21 @@ import { Paragraph, Title } from '../../UI/typography';
 import { column1, column2, column3 } from './mock';
 import { ColumnList, ColumnListItem, Grid, ParagraphSlogan, StyledFooter, TitleBrand } from './styles';
 
-const Thanks = styled.div`
-  display: grid;
-  grid-template-columns: max-content 1fr;
-  column-gap: 15px;
-  align-items: center;
-  font-size: 0.7rem;
-`;
-
 export const Footer = () => (
   <StyledFooter>
     <Container>
       <Grid>
         <div>
-          <TitleBrand bold size='1.5rem' margin='15px'>
+          <TitleBrand bold size='1.5rem' margin='0 0 15px 0'>
             donor.md
           </TitleBrand>
-          <ParagraphSlogan size='.9rem' margin='15px'>
+          <ParagraphSlogan size='.9rem' margin='0 0 15px 0'>
             Сервис, созданный с концепцией «Люди помогают людям» для формирования стабильной базы крови,
             восполняемой регулярными донорами.
           </ParagraphSlogan>
         </div>
         <div>
-          <Title as='h3' margin='15px'>
+          <Title as='h3' margin='0 0 15px 0'>
             О донорстве
           </Title>
           <ColumnList>
@@ -44,7 +36,7 @@ export const Footer = () => (
           </ColumnList>
         </div>
         <div>
-          <Title as='h3' margin='15px'>
+          <Title as='h3' margin='0 0 15px 0'>
             О нас
           </Title>
           <ColumnList>
@@ -58,7 +50,7 @@ export const Footer = () => (
           </ColumnList>
         </div>
         <div>
-          <Title as='h3' margin='15px'>
+          <Title as='h3' margin='0 0 15px 0'>
             Сотрудничество
           </Title>
           <ColumnList>
@@ -74,7 +66,7 @@ export const Footer = () => (
       </Grid>
       <Thanks>
         <Image src='/images/eu-flag.png' width={47} height={38} layout='fixed' />
-        <Paragraph color='textMuted'>
+        <Paragraph color='textMuted' margin='0'>
           Данный web-сервис запущен при финансовой поддержке Европейского Союза. Содержание сервиса является
           предметом ответственности команды donor.md и не отражает точку зрения Европейского Союза.
         </Paragraph>
@@ -82,3 +74,11 @@ export const Footer = () => (
     </Container>
   </StyledFooter>
 );
+
+const Thanks = styled.div`
+  display: grid;
+  grid-template-columns: max-content 1fr;
+  column-gap: 15px;
+  align-items: center;
+  font-size: 0.7rem;
+`;
